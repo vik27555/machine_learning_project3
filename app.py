b0d0fa5167bef0553a9642b3beb0f00bc7b31b86
vik27555 <101319147+vik27555@users.noreply.github.com>
2022-06-17 09:58:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "starting machine learning project"
+    return "starting machine learning project successfull project"
 
 
 if __name__=="__main__":
